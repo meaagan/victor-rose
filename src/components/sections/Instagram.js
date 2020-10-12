89df8fcc49cbbdc875563479a6f28acd7a314190
Meagan Butters <meagan.butters@hotmail.com>
2020-10-12 18:08:45 -0400

    Instagram carousel

--- src/components/sections/Instagram.js
+++ src/components/sections/Instagram.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby';
-import styled from 'styled-components'
 import Img from 'gatsby-image'
 import { Section, Container } from '@components/global';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 const Instagram = () => {
     const data = useStaticQuery(graphql`
@@ -11,35 +12,63 @@ const Instagram = () => {
             edges {
                 node {
                 id
-                original
+                preview
                 localFile {
                     childImageSharp {
-                    fixed(width: 150, height: 150) {
+                    fixed(width: 200, height: 200) {
                         ...GatsbyImageSharpFixed
                     }
-                    }
                 }
-                }
-                }
+              }
+              original
             }
+          }
         }
+      }
     `)
         
     const instaPosts = data.allInstaNode.edges
 
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1024 },
+          items: 4,
+          slidesToSlide: 3 // optional, default to 1.
+        },
+        tablet: {
+          breakpoint: { max: 1024, min: 464 },
+          items: 2,
+          slidesToSlide: 2 // optional, default to 1.
+        },
+        mobile: {
+          breakpoint: { max: 464, min: 0 },
+          items: 1,
+          slidesToSlide: 1 // optional, default to 1.
+        }
+      };
+
     return (
-        <Section id="about">
+        <Section id="instagram">
             <Container>
-                <h1>Hello</h1>
-                <ul>
+                <h2>Follow us on Instagram</h2>
+                <Carousel
+                    swipeable={true}
+                    centerMode={true}
+                    responsive={responsive}
+                    ssr={true} // means to render carousel on server-side.
+                    keyBoardControl={true}
+                    customTransition='all .5'
+                    transitionDuration={500}
+                    removeArrowOnDeviceType={["tablet", "mobile"]}
+                >
                     {instaPosts.map(({ node }) => {
                         return(
-                            <li key ={node.id}>
+                            <div>
                                 <Img fixed = {node.localFile.childImageSharp.fixed} />
-                            </li>
+                            </div>
                         )
                     })}
-                </ul>
+                </Carousel>
             </Container>
         </Section>
     )
