ba85bc5d789d59e4982d6ff7a9ef1cdb07a361d8
Meagan Butters <meagan.butters@hotmail.com>
2020-11-21 12:56:06 -0500

    French about section on about us page

---
+++ src/components/sections/about/AboutUs.fr.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutUs = () => {
+    return(
+        <div style={{width: "80%", margin: '0 auto'}}>
+            <h1 style={{textAlign:'center', margin: '10px'}}>À PROPOS DE NOUS</h1>
+            <p style={{textAlign:'justify'}} >Créé en 2015, le Victor Rose Espresso Bar est un lieu de rassemblement de quartier engagé à servir du café équitable direct et raffiné, ainsi qu’à supporter des artisans locaux. Le duo mère-fille Terry et Marie a nommé le café en l’honneur de leur (arrière-) grand-père Victor Rose, en mémoire de son amour pour l’art, la justice sociale et le vivre-ensemble .</p><br />
+            <p style={{textAlign:'justify'}}>Bar Espresso Victor Rose reconnaît qu'il est situé sur le territoire traditionnel des nations Anishinaabeg et Kanien'keha:ke, ainsi qu'à un endroit ayant longtemps servi de lieu de rencontres et d'échanges entre diverses nations autochtones.</p>
+        </div>
+    )
+}
+
+export default AboutUs
